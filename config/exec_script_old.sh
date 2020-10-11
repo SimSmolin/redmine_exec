bade9f8fd5e3e1cccb058d4f6e117c97d329451f
sergey <sergey.melnikov@krit.pro>
2020-10-11 17:40:38 +0400

    Bug - не сохранялось занесенное руками влемя "согласовано"

---
+++ config/exec_script_old.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pwd
+echo $1
+ls -l
+#exit 0
--- init.rb
+++ init.rb
@@ -10,9 +10,11 @@ end
 
 Redmine::Plugin.register :redmine_exec do
   name 'Redmine Exec plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Sergey Melnikov'
+  description 'execute shell script and download result'
+  version '0.0.2'
   url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author_url 'https://github.com/SimSmolin'
+
+  settings :default => {'empty' => true}, :partial => 'settings/plugin_setting'
 end
